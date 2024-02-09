db692a962703a3bca5e941b96e485f2f33d24959
patinkaew <pinkaew@dx1-915-hep.hip.helsinki.fi>
2024-02-09 15:05:56 +0200

    modified compareLiteHLT for AlCaJet Nano

---
+++ mk_compareLiteHLT2.C
@@ -0,0 +1,18 @@
+{
+
+  //gROOT->ProcessLine(".exception");
+
+  gROOT->ProcessLine(".L tools.C+g");
+
+  // Link JEC libraries that are modified to work stand-alone
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/Utilities.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/JetCorrectorParameters.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/SimpleJetCorrector.cc+");
+  gROOT->ProcessLine(".L CondFormats/JetMETObjects/src/FactorizedJetCorrector.cc+");
+
+  gROOT->ProcessLine(".L compareLiteHLT2.C+g");
+  //gROOT->ProcessLine(".L drawCompareLite.C+g");
+
+  compareLiteHLT2("2023D");
+  //drawCompareLite("2022F");
+}
